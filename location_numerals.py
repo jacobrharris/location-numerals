59f8fa145539ee630d670c3fa073203a1e4687fa
Jacob Harris <jacob@jacobharris.org>
2016-06-05 18:29:07 -0400

    added #2

--- location_numerals.py
+++ location_numerals.py
@@ -42,8 +42,11 @@ def integers_to_keep(x):
 def integers_to_logs(int_list):
 	log_list = []
 	for integer in int_list:
-		log = int(math.log(integer, 2))
-		log_list.append(log)
+		if integer == 0:
+			log_list.append(0)
+		else:
+			log = int(math.log(integer, 2))
+			log_list.append(log)
 	
 	return log_list
 
@@ -56,11 +59,38 @@ def logs_to_string(alphabet, log_list):
 	
 	return "".join(letters_list)
 
+# Convert alphabet to list of logs
+def alphabet_to_logs(alphabet):
+	logs_list = []
+	for letter in alphabet:
+		log = alphabet.index(letter)
+		logs_list.append(log)
+	
+	return logs_list
+	
+def string_to_integer(alphabet, string):
+	string_list = [letter for letter in string]
+	integers_list = []
+	for letter in string_list:
+		integer = alphabet.index(letter)
+		integers_list.append(integer)
+
+	logs_list = integers_to_logs(integers_list)
+	newInteger = 0
+	for integer in integers_list:
+		newInteger += 2 ** integer 
+	
+	return newInteger
+
 if __name__ == '__main__':
 	# Set up letter objects
 	alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+	# 1
 	integers_list = integers_to_keep(87)
 	logs_list = integers_to_logs(integers_list)
 	letters_list = logs_to_string(alphabet, logs_list)
 	print letters_list
+	
+	# 2	
+	print string_to_integer(alphabet, 'ad')
